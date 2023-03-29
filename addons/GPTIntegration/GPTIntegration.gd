f2e423af87d0ce49e9de8f0bb4747d8630a7b13d
FinepointCGI <finepointcgi@gmail.com>
2023-03-29 09:32:22 -0400

    Updated to follow Godot Standards
    Adjusted to add Chat Bubbles and more chat engines.

--- addons/GPTIntegration/GPTIntegration.gd
+++ addons/GPTIntegration/GPTIntegration.gd
@@ -8,118 +8,139 @@ enum modes {
 	Help
 }
 
-var apiKey = ""
+var api_key = ""
 var max_tokens = 1024
 var temperature = 0.5
 var url = "https://api.openai.com/v1/completions"
-var headers : Array
+var headers = ["Content-Type: application/json", "Authorization: Bearer " + api_key]
 var engine = "text-davinachi-003"
-var chatDock
-var httpRequest
-var currentMode
-var cursorPos
-var codeEditor
-var settingsMenu
-
-func update_headers():
-	headers = ["Content-Type: application/json", "Authorization: Bearer " + apiKey]
+var chat_dock
+var http_request
+var current_mode
+var cursor_pos
+var code_editor
+var settings_menu
 
 func _enter_tree():
-	chatDock = preload("res://addons/GPTIntegration/Chat.tscn").instantiate()
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, chatDock)
+	chat_dock = preload("res://addons/GPTIntegration/Chat.tscn").instantiate()
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, chat_dock)
 	
-	chatDock.get_node("Button").connect("pressed", onChatButtonDown)
-	chatDock.get_node("HBoxContainer/Action").connect("pressed", onActionButtonDown)
-	chatDock.get_node("HBoxContainer/Help").connect("pressed", onHelpButtonDown)
-	chatDock.get_node("HBoxContainer/Summary").connect("pressed", onSummaryButtonDown)
+	chat_dock.get_node("Button").connect("pressed", _on_chat_button_down)
+	chat_dock.get_node("HBoxContainer/Action").connect("pressed", _on_action_button_down)
+	chat_dock.get_node("HBoxContainer/Help").connect("pressed", _on_help_button_down)
+	chat_dock.get_node("HBoxContainer/Summary").connect("pressed", _on_summary_button_down)
 	# Initialization of the plugin goes here.
-	add_tool_menu_item("GPT Chat", onShowSettings)
-	loadSettings()
-	update_headers()
+	add_tool_menu_item("GPT Chat", on_show_settings)
+	load_settings()
 	pass
 
-func onShowSettings():
-	settingsMenu = preload("res://addons/GPTIntegration/SettingsWindow.tscn").instantiate()
-	settingsMenu.get_node("Control/Button").connect("pressed", onSettingsButtonDown)
-	setSettings(apiKey, int(max_tokens), float(temperature), engine)
-	add_child(settingsMenu)
-	settingsMenu.connect("close_requested", settingsMenuClose)
-	settingsMenu.popup()
+func on_show_settings():
+	settings_menu = preload("res://addons/GPTIntegration/SettingsWindow.tscn").instantiate()
+	settings_menu.get_node("Control/Button").connect("pressed", on_settings_button_down)
+	set_settings(api_key, int(max_tokens), float(temperature), engine)
+	add_child(settings_menu)
+	settings_menu.connect("close_requested", settings_menu_close)
+	settings_menu.popup()
 	
-func onSettingsButtonDown():
-	var index : int
-	apiKey = settingsMenu.get_node("HBoxContainer/VBoxContainer2/APIKey").text
-	update_headers()
-	max_tokens = int(settingsMenu.get_node("HBoxContainer/VBoxContainer2/MaxTokens").text)
-	temperature = float(settingsMenu.get_node("HBoxContainer/VBoxContainer2/Temperature").text)
-	index = settingsMenu.get_node("HBoxContainer/VBoxContainer2/OptionButton").selected
+func on_settings_button_down():
+	api_key = settings_menu.get_node("HBoxContainer/VBoxContainer2/APIKey").text
+	max_tokens = int(settings_menu.get_node("HBoxContainer/VBoxContainer2/MaxTokens").text)
+	temperature = float(settings_menu.get_node("HBoxContainer/VBoxContainer2/Temperature").text)
+	var index = settings_menu.get_node("HBoxContainer/VBoxContainer2/OptionButton").selected
 	
-	if index == 0:
-		engine = "text-davinchi-003"
-	settingsMenuClose()
+	if engine == "text-davinchi-003":
+		index = 0
+	elif index == 1:
+		engine = "code-davinci-002"
+	elif index == 2:
+		engine = "text-curie-001"
+	elif index == 3: 
+		engine = "text-babbage-001"
+	elif index == 4:
+		engine = "text-ada-001"
+	elif index == 5:
+		engine = "code-cushman-002"
+		
+	settings_menu_close()
 	pass
 
-func settingsMenuClose():
-	settingsMenu.queue_free()
+func settings_menu_close():
+	settings_menu.queue_free()
 	pass
 
-func setSettings(apikey, maxtokens, temp, engine):
-	settingsMenu.get_node("HBoxContainer/VBoxContainer2/APIKey").text = apikey
-	settingsMenu.get_node("HBoxContainer/VBoxContainer2/MaxTokens").text = var_to_str(maxtokens)
-	settingsMenu.get_node("HBoxContainer/VBoxContainer2/Temperature").text = var_to_str(temp)
+# This GDScript code sets the settings in a settings
+# menu. It sets the API key, the maximum number of tokens,
+# the temperature, and the engine. The engine is set
+# by selecting the corresponding ID from a list of options.
+func set_settings(api_key, maxtokens, temp, engine):
+	settings_menu.get_node("HBoxContainer/VBoxContainer2/APIKey").text = api_key
+	settings_menu.get_node("HBoxContainer/VBoxContainer2/MaxTokens").text = str(maxtokens)
+	settings_menu.get_node("HBoxContainer/VBoxContainer2/Temperature").text = str(temp)
 	var id = 0
+	
 	if engine == "text-davinchi-003":
 		id = 0
-	settingsMenu.get_node("HBoxContainer/VBoxContainer2/OptionButton").selected = id
+	elif  engine == "code-davinci-002":
+		id = 1
+	elif engine == "text-curie-001":
+		id = 2
+	elif engine == "text-babbage-001":
+		id = 3
+	elif engine == "text-ada-001":
+		id = 4
+	elif engine == "code-cushman-002":
+		id = 5
+		
+	settings_menu.get_node("HBoxContainer/VBoxContainer2/OptionButton").select(id)
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
-	remove_control_from_docks(chatDock)
-	chatDock.queue_free()
+	remove_control_from_docks(chat_dock)
+	chat_dock.queue_free()
 	remove_tool_menu_item("GPT Chat")
-	saveSettings()
+	save_settings()
 	pass
 
 func _ready():
-	httpRequest = HTTPRequest.new()
-	add_child(httpRequest)
-	httpRequest.connect("request_completed", onRequestCompleted)
+	http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", _on_request_completed)
 
-func onChatButtonDown():
-	if(chatDock.get_node("TextEdit").text != ""):
-		currentMode = modes.Chat
-		var prompt = chatDock.get_node("TextEdit").text
-		chatDock.get_node("TextEdit").text = ""
-		AddToChat("Me: " + prompt)
-		CallGPT(prompt)
+func _on_chat_button_down():
+	if(chat_dock.get_node("TextEdit").text != ""):
+		current_mode = modes.Chat
+		var prompt = chat_dock.get_node("TextEdit").text
+		chat_dock.get_node("TextEdit").text = ""
+		add_to_chat("Me: " + prompt)
+		call_GPT(prompt)
 		
-func CallGPT(prompt):
+func call_GPT(prompt):
 	var body = JSON.new().stringify({
 		"prompt": prompt,
 		"temperature": temperature,
 		"max_tokens": max_tokens,
 		"model": "text-davinci-003"
 	})
-	var error = httpRequest.request(url, headers, HTTPClient.METHOD_POST, body)
+	var error = http_request.request(url, headers, HTTPClient.METHOD_POST, body)
 	
 	if error != OK:
 		push_error("Something Went Wrong!")
 	
-func onActionButtonDown():
-	currentMode = modes.Action
-	CallGPT("Code the following prompt in GDscript for the Godot game engine: " + GetSelectedCode())
+func _on_action_button_down():
+	current_mode = modes.Action
+	call_GPT("Code this for Godot " + get_selected_code())
 	
-func onHelpButtonDown():
-	currentMode = modes.Help
-	var code = GetSelectedCode()
-	chatDock.get_node("TextEdit").text = ""
-	AddToChat("Me: " + "What is wrong with this GDScript code? " + code)
-	CallGPT("What is wrong with this GDScript code? " + code)
+func _on_help_button_down():
+	current_mode = modes.Help
+	var code = get_selected_code()
+	chat_dock.get_node("TextEdit").text = ""
+	add_to_chat("Me: " + "What is wrong with this GDScript code? " + code)
+	call_GPT("What is wrong with this GDScript code? " + code)
 	
-func onSummaryButtonDown():
-	currentMode = modes.Summarise
-	CallGPT("Summarize this GDScript Code: " + GetSelectedCode())
-	print("onSummaryButtonDown Pressed")
+func _on_summary_button_down():
+	current_mode = modes.Summarise
+	call_GPT("Summarize this GDScript Code " + get_selected_code())
+	print("_on_summary_button_down Pressed")
 
 
 # This GDScript code is used to handle the response from
@@ -130,16 +151,16 @@ func onSummaryButtonDown():
 # insert it into the code editor as a summarised version
 # with each line having a maximum length of 50 characters
 # and each line starting with a "#".
-func onRequestCompleted(result, responseCode, headers, body):
+func _on_request_completed(result, responseCode, headers, body):
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
 	print(response)
 	
 	var newStr = response.choices[0].text
-	if currentMode == modes.Chat:
-		AddToChat("GPT: " + newStr)
-	elif currentMode == modes.Summarise:
+	if current_mode == modes.Chat:
+		add_to_chat("GPT: " + newStr)
+	elif current_mode == modes.Summarise:
 		var str = response.choices[0].text.replace("\n" , "")
 		newStr = "# "
 		var lineLength = 50
@@ -151,48 +172,62 @@ func onRequestCompleted(result, responseCode, headers, body):
 			else:
 				newStr += str[i]
 				currentLineLength += 1
-		codeEditor.insert_line_at(cursorPos, newStr)
-	elif currentMode == modes.Action:
-		codeEditor.insert_line_at(cursorPos, newStr)
-	elif currentMode == modes.Help:
-		AddToChat("GPT: " + response.choices[0].text)
+		code_editor.insert_line_at(cursor_pos, newStr)
+	elif current_mode == modes.Action:
+		code_editor.insert_line_at(cursor_pos, newStr)
+	elif current_mode == modes.Help:
+		add_to_chat("GPT: " + response.choices[0].text)
 	pass
 	
-func GetSelectedCode():
+func get_selected_code():
 	var currentScriptEditor = get_editor_interface().get_script_editor().get_current_editor()
 	
-	codeEditor = currentScriptEditor.get_base_editor()
+	code_editor = currentScriptEditor.get_base_editor()
 	
-	if currentMode == modes.Summarise:
-		cursorPos = codeEditor.get_selection_from_line()
-	elif currentMode == modes.Action:
-		cursorPos = codeEditor.get_selection_to_line()
-	return codeEditor.get_selected_text()
+	if current_mode == modes.Summarise:
+		cursor_pos = code_editor.get_selection_from_line()
+	elif current_mode == modes.Action:
+		cursor_pos = code_editor.get_selection_to_line()
+	return code_editor.get_selected_text()
 
-func AddToChat(text):
-	chatDock.get_node("RichTextLabel").text += "\n" + text + "\n"
+func add_to_chat(text):
+	var chat_bubble = preload("res://addons/GPTIntegration/ChatBubble.tscn").instantiate()
+	chat_bubble.get_node("RichTextLabel").text = "\n" + text + "\n"
+	chat_dock.get_node("ScrollContainer/VBoxContainer").add_child(chat_bubble)
+	#chat_dock.get_node("RichTextLabel").text += "\n" + text + "\n"
 
-func saveSettings():
+# This GDScript code creates a JSON file containing the
+# values of the variables "api_key", "max_tokens", "temperature",
+# and "engine", and stores the file in the "res://addons/GPTIntegration/"
+# directory.
+func save_settings():
+	
 	var data ={
-		"api_key":apiKey,
+		"api_key":api_key,
 		"max_tokens": max_tokens,
 		"temperature": temperature,
 		"engine": engine
 	}
+	print(data)
 	var jsonStr = JSON.stringify(data)
 	var file = FileAccess.open("res://addons/GPTIntegration/settings.json",FileAccess.WRITE)
 	
 	file.store_string(jsonStr)
 	file.close()
 
-func loadSettings():
+# This GDScript code opens a JSON file, parses the data
+# from it, and assigns the values to variables. The variables
+# are api_key, max_tokens, temperature, and engine.
+func load_settings():
 	var file = FileAccess.open("res://addons/GPTIntegration/settings.json",FileAccess.READ)
 
 	var jsonStr = file.get_as_text()
 	file.close()
 	var data = JSON.parse_string(jsonStr)
-	
-	apiKey = data["api_key"]
+	print(data)
+	api_key = data["api_key"]
 	max_tokens = int(data["max_tokens"])
 	temperature = float(data["temperature"])
 	engine = data["engine"]
+
+
